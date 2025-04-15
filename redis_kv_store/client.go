6fcb214abcd18f7310764efe9289f80b0fb55442
mshahrad <mshahrad@node0.pileus.praxis-pg0.clemson.cloudlab.us>
2025-04-15 18:30:37 -0400

    the sharding configuration added

--- redis_kv_store/client.go
+++ redis_kv_store/client.go
@@ -2,55 +2,125 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"math/rand"
+	"encoding/json"
 	"pileus/redis"
+	"github.com/google/uuid"
 )
 
-type foo struct {
-	Bar string
+type Record struct {
+    Key   int `json:"key"`
+    Value string `json:"value"`
+}
+
+// The logic for sharding based on the shard_config
+type Shard struct {
+	RangeStart int `json:"start"`
+	RangeEnd   int `json:"end"`
+	PrimaryAddress	string `json:"primary"` 
+	Primary	redis.Client `json:"-"`	
+}
+
+type Config struct {
+	Shards []Shard `json:"shards"`
+}
+
+// Define the Global varibales: config, redis.Client
+var Node0_Store redis.Client
+var GlobalConfig *Config
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	// Initialize Redis clients for each shard
+	for i, shard := range config.Shards {
+	
+		opts := redis.DefaultOptions
+		opts.Address = shard.PrimaryAddress
+
+		client, err := redis.NewClient(opts)
+		if err != nil {
+			return nil, fmt.Errorf("failed to connect to redis at %s: %w", shard.PrimaryAddress, err)
+		}
+
+		config.Shards[i].Primary = client
+	}
+
+	return &config, nil
 }
 
 func main() {
-	options := redis.DefaultOptions 
-	options.Address = "130.127.133.5:6379"
 
-	// Create client
-	client, err := redis.NewClient(options)
+	// Load the sharding config 
+	var err error
+
+	GlobalConfig, err = LoadConfig("sharding_config.json")
 	if err != nil {
 		panic(err)
 	}
-	defer client.Close()
 
-	// Store, retrieve, print and delete a value
-	interactWithStore(client)
+	fmt.Printf("Loaded config: %+v\n", GlobalConfig)
+
+	putWorkload(20)
 }
 
-// interactWithStore stores, retrieves, prints and deletes a value.
-// It's completely independent of the store implementation.
-func interactWithStore(store redis.Client) {
-	// Store value
-	// val := foo{
-	// 	Bar: "baz",
-	// }
-	// err := store.Set("foo1", val)
-	// if err != nil {
-	// 	panic(err)
-	// }
+func putWorkload(count int) error {
+	fmt.Printf("Entered the putworkalod function\n")
 
-	// Retrieve value
-	retrievedVal := new(foo)
-	found, err := store.Get("foo1", retrievedVal)
-	if err != nil {
-		panic(err)
-	}
-	if !found {
-		panic("Value not found")
-	}
+    for i := 1; i <= count; i++ {
+		// Randomly generate the key and value
+        key := rand.Intn(6000)
+		value := uuid.New().String()
 
-	fmt.Printf("foo: %+v\n", *retrievedVal) // Prints `foo: {Bar:baz}`
+        rec := Record{
+            Key:   key,
+            Value: value,
+        }
 
-	// Delete value
-	// err = store.Delete("foo1")
-	// if err != nil {
-	// 	panic(err)
-	// }
+		fmt.Printf("Record: %+v\n", rec)
+
+        err := sendPut(rec)
+        if err != nil {
+            return fmt.Errorf("failed to sendPut for key %s: %w", key, err)
+        }
+    }
+    return nil
+}
+
+func sendPut(rec Record) error {
+	fmt.Printf("Entered the sendPut function\n")
+    shardID := determineShardForKey(rec.Key)
+	fmt.Printf("shardId is %d \n", shardID)
+
+	var err error
+	err = GlobalConfig.Shards[shardID].Primary.Set(strconv.Itoa(rec.Key), rec.Value)
+    if err != nil {
+		fmt.Printf("Failed with the write\n")
+		fmt.Printf("%w \n", err)
+        return fmt.Errorf("failed to store key %d: %w", rec.Key, err)
+    }
+    return nil
+}
+
+func determineShardForKey(key int) int {
+    fmt.Printf("Entered determineShardForKey with key %d\n", key)
+   
+    for i, shard := range GlobalConfig.Shards {
+    
+        if key >= shard.RangeStart && key <= shard.RangeEnd {
+            fmt.Printf("Key %d belongs to shard #%d: %+v\n", key, i, shard)
+            return i
+        }
+    }
+    panic(fmt.Sprintf("No shard found for key: %d", key))
 }
