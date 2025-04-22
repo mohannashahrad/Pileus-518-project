931bb89c2ea699901f2267e31fe13be851dfa3d4
mshahrad <mshahrad@utah.pileus-stores.praxis-pg0.utah.cloudlab.us>
2025-04-22 11:58:53 -0600

    set ip script for initializing on a server

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update 
+
+# Install docker and docker-compose
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+
+# For testing
+sudo apt-get install golang
